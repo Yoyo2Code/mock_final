d29d768af94911d950ac10048087e780544eb1ff
Yoseph Anderson <yrganderson@gmail.com>
2016-10-31 11:03:42 -0600

    user can save a link

--- spec/feature/user_can_submit_a_link_spec.rb
+++ spec/feature/user_can_submit_a_link_spec.rb
@@ -8,6 +8,13 @@ describe "User", :type => :feature do
       page.set_rack_session(user_id: user.id)
 
       visit '/'
+
+      expect(page).to have_content('Links Index')
+
+      fill_in :title, with: "Google"
+      fill_in :url, with: "http://www.google.com"
+
+      click_on "Create Link"
     end
   end
 end
