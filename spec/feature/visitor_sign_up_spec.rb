e16c31c344572100ab0ce7d38e13dd613a3237bc
Yoseph Anderson <yrganderson@gmail.com>
2016-10-31 10:24:55 -0600

    signup password confirmation

--- spec/feature/visitor_sign_up_spec.rb
+++ spec/feature/visitor_sign_up_spec.rb
@@ -11,10 +11,27 @@ describe "Unauthorized visitor", :type => :feature do
 
       fill_in :Username, with: "username"
       fill_in :Password, with: "newPassword"
+      fill_in :user_password_confirmation, with: "newPassword"
 
       click_on 'Create User'
 
       expect(page).to have_content("Username already in use")
+    end
+
+    it "should redirect to links in index page if successful" do
+      user = create(:user)
+
+      visit '/login'
+
+      click_on 'Sign Up'
+
+      fill_in :Username, with: "newuser"
+      fill_in :Password, with: "newPassword"
+      fill_in :user_password_confirmation, with: "newPassword"
+
+      click_on 'Create User'
+
+      expect(page).to have_content("Links Index")
     end
   end
 end
